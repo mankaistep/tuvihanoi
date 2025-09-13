8f86b540a08efff9ca349f272f8ea84b9e12f6e3
mankaistep <hhoang21vn@gmail.com>
2025-09-13 15:29:00 +0700

    optimize prompt

--- src/app/utils/gpt_utils.js
+++ src/app/utils/gpt_utils.js
@@ -7,8 +7,6 @@ const openai = new OpenAI({
 
 
 export async function runPrompt(prompt) {
-  console.log('Prompt: ', prompt)
-
   // Count miliseconds and cost
   const start = Date.now();
   const response = await openai.chat.completions.create({
@@ -19,8 +17,7 @@ export async function runPrompt(prompt) {
     ],
   });
   const end = Date.now();
-  console.log('Time: ', end - start);
-  console.log('Cost: ', response.usage.total_tokens);
+  console.log('Sent a request, response time: ', end - start, 'ms, cost:', response.usage.total_tokens, 'tokens');
 
   return response.choices[0]?.message?.content?.trim() || "";
 }
